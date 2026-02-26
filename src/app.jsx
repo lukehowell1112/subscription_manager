dd882f19a296c8a397f72700f19cc838d90f555f
lukehowell1112 <lukehowell1112@gmail.com>
2026-02-26 16:23:11 -0700

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -13,65 +13,69 @@ import { Edit_Sub } from './edit_sub/edit_sub';
 import { Edit_Form } from './edit_form/edit_form';
 import { About } from './about/about';
 import { RequireAuth } from "./auth/RequireAuth";
+import { getCurrentUser, logout } from './services/subscriptionService';
+
 
 export default function App() {
-  return (
-    <BrowserRouter>
-    <div className="body">
-        <header className="site-header">
-            <div className="container">
-                <div className="header-row">
-                    <div className="brand">
-                        <img src="favicon.ico" alt="Subscription Manager icon" className="brand-icon"/>
-                        <h1>Subscription Manager<sup>&reg;</sup></h1>
+    const user = getCurrentUser();
+    
+    return (
+        <BrowserRouter>
+        <div className="body">
+            <header className="site-header">
+                <div className="container">
+                    <div className="header-row">
+                        <div className="brand">
+                            <img src="favicon.ico" alt="Subscription Manager icon" className="brand-icon"/>
+                            <h1>Subscription Manager<sup>&reg;</sup></h1>
+                        </div>
+
+                        <nav className="site-nav">
+                            <menu className="nav-list">
+                                <li><NavLink className="nav-link" to="/">Home</NavLink></li>
+                                <li><NavLink className="nav-link" to="dashboard">Dashboard</NavLink></li>
+                                <li className="dropdown">
+                                    <a
+                                        className="nav-link dropdown-toggle"
+                                        href="#"
+                                        role="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >Edit Dashboard</a>
+
+                                    <ul className="dropdown-menu dropdown-menu-dark">
+                                        <li><NavLink className="dropdown-item" to="add">Add Subscription</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="edit_sub">Edit Subscription</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="delete">Delete Subscription</NavLink></li>
+                                    </ul>
+                                </li>
+                                <li><NavLink className="nav-link" to="about">About</NavLink></li>
+                            </menu>
+                        </nav>
                     </div>
+                </div>    
+            </header>
 
-                    <nav className="site-nav">
-                        <menu className="nav-list">
-                            <li><NavLink className="nav-link" to="/">Home</NavLink></li>
-                            <li><NavLink className="nav-link" to="dashboard">Dashboard</NavLink></li>
-                            <li className="dropdown">
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >Edit Dashboard</a>
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} />
+            <Route path='/add' element={<RequireAuth><Add /></RequireAuth>} />
+            <Route path='/delete' element={<RequireAuth><Delete /></RequireAuth>} />
+            <Route path='/edit_sub' element={<RequireAuth><Edit_Sub /></RequireAuth>} />
+            <Route path='/edit_form' element={<RequireAuth><Edit_Form /></RequireAuth>} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
 
-                                <ul className="dropdown-menu dropdown-menu-dark">
-                                    <li><NavLink className="dropdown-item" to="add">Add Subscription</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to="edit_sub">Edit Subscription</NavLink></li>
-                                    <li><NavLink className="dropdown-item" to="delete">Delete Subscription</NavLink></li>
-                                </ul>
-                            </li>
-                            <li><NavLink className="nav-link" to="about">About</NavLink></li>
-                        </menu>
-                    </nav>
+            <footer className="site-footer">
+                <div className="container footer-row">
+                    <span className="text-reset">Created by Luke Howell</span>
+                    <NavLink to="https://github.com/lukehowell1112/subscription_manager">GitHub</NavLink>
                 </div>
-            </div>    
-        </header>
+            </footer>
 
-      <Routes>
-        <Route path='/' element={<Login />} exact />
-        <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} />
-        <Route path='/add' element={<RequireAuth><Add /></RequireAuth>} />
-        <Route path='/delete' element={<RequireAuth><Delete /></RequireAuth>} />
-        <Route path='/edit_sub' element={<RequireAuth><Edit_Sub /></RequireAuth>} />
-        <Route path='/edit_form' element={<RequireAuth><Edit_Form /></RequireAuth>} />
-        <Route path='/about' element={<About />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-
-        <footer className="site-footer">
-            <div className="container footer-row">
-                <span className="text-reset">Created by Luke Howell</span>
-                <NavLink to="https://github.com/lukehowell1112/subscription_manager">GitHub</NavLink>
-            </div>
-        </footer>
-
-    </div>
-    </BrowserRouter>
+        </div>
+        </BrowserRouter>
   );
 }
 
