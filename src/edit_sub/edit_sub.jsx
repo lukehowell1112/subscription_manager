e3f5c6379b8c0474c521a0a737e995f68dbff6e4
luke.howell <lukehowell1112@gmail.com>
2026-03-12 19:09:34 -0600

    Update edit_sub.jsx

--- src/edit_sub/edit_sub.jsx
+++ src/edit_sub/edit_sub.jsx
@@ -8,7 +8,9 @@ export function Edit_Sub() {
 	const [selectedId, setSelectedId] = useState(null);
 
 	useEffect(() => {
-		fetch("http://localhost:4000/api/subscriptions")
+		fetch("http://localhost:4000/api/subscriptions", {
+			credentials: "include",
+		})
 			.then((res) => res.json())
 			.then((subs) => {
 				setSubscriptions(subs);
